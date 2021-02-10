4c055d95287360d9d98c021be348a917409fc03e
rodrigodojo <rodrigodoj@gmail.com>
2021-02-10 17:15:04 -0300

    Exemplo de reuso com Object

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,13 +10,13 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		PrintServiceString ps = new PrintServiceString();
+		PrintService ps = new PrintService();
 		
 		System.out.print("How many values?");
 		int n = sc.nextInt();
 		
 		for (int i=0 ;i<n; i++) {
-			String value = sc.next();
+			Integer value = sc.nextInt();
 			ps.addValue(value);
 		}
 		
